71f7239c3db8449c43d68ed92a493d290f889d70
Ryder-Lin1 <aceryderlin@gmail.com>
2024-09-04 23:09:10 -0400

    Initial commit

---
+++ Q2.py
@@ -0,0 +1,17 @@
+hashtable1={
+    "poblano":1500,
+    "mirasol":6000,
+    "serrano":15500,
+    "cayenne":40000,
+    "thai":75000,
+    "habanero":125000
+}
+ts = 0
+x = int(input())
+for y in range(0,x):
+    y = input()
+    if hashtable1[y]== hashtable1[y]:
+        ts = ts+ hashtable1[y]
+
+print(ts)
+    
